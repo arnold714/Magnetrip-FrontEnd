972f3c7d4dd6bb329d3690787a421ec946fc269c
wjdrbgus8167 <wjdrbgus8167@naver.com>
2024-11-20 15:35:55 +0900

    FEAT:공지사항

--- src/components/notices/TripNoticesDetail.vue
+++ src/components/notices/TripNoticesDetail.vue
@@ -51,11 +51,7 @@ function onDeleteArticle() {
 <template>
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-lg-10">
-        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
-          <mark class="sky">공지사항</mark>
-        </h2>
-      </div>
+      
       <div class="col-lg-10 text-start">
         <div class="row my-2">
           <h2 class="text-secondary px-5">
@@ -65,17 +61,11 @@ function onDeleteArticle() {
         <div class="row">
           <div class="col-md-8">
             <div class="clearfix align-content-center">
-              <img
-                class="avatar me-2 float-md-start bg-light p-2"
-                src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"
-                alt="Author"
-              />
+           
             
             </div>
           </div>
-          <div class="col-md-4 align-self-center text-end">
-            댓글: 17
-          </div>
+          
         </div>
         <div class="divider mb-3"></div>
         <!-- Render Content with HTML -->
